ea52f3ad9a9eb902de5c3b2366c91c49478cc436
user <user@WIN-MHGL1PRC8MH>
2024-03-31 17:01:02 +0300

    1

--- main3.py
+++ main3.py
@@ -61,6 +61,8 @@ def one():
 
     
     length=0
+    new_sum=0
+    new1_sum=0
                         
     
     
@@ -68,8 +70,36 @@ def one():
         length+=i
         if(order.index(i)!=0 and order.index(i)!=1):        
             new=next(can_iter)
+            new_sum+=new
             new1=next(can1_iter)
-            if(i<new and i<new1):
+            new1_sum+=new1
+
+        
+            if(wait_s==False):
+                if(sum_c<sum_c1):
+                    wait.append(sum_c)
+                    can.append(sum_c)
+                    ready.append(sum_c)
+                    wait_s=True
+                
+                else:
+                    wait.append(sum_c1)
+                    can.append(sum_c1)
+                    ready.append(sum_c1)
+                    wait_s=True
+
+            if(wait1_s==False and wait_s==True):
+                if(sum_c<sum_c1):
+                    wait1.append(sum_c)
+                    wait.append(sum_c)
+                    wait1_s=True
+                else:
+                    wait1.append(sum_c1)
+                    wait.append(sum_c1)
+                    wait1_s=True
+
+
+            if(length<new_sum or length<new1_sum):
 
                 if(wait_s==True):
                     wait.append(length)
@@ -82,7 +112,12 @@ def one():
                 else:
                     bad.append(length)
 
-            elif(i>=new):
+            
+
+
+
+            '''
+            elif(i>=new_sum):
                 if(wait_s==False):
                     wait.append(length)
                     wait_s=True
@@ -93,7 +128,30 @@ def one():
                 
                 ready.append(length)
 
-            elif(i==new1):
+            elif(i>=new1_sum):
+                if(wait_s==False):
+                    wait.append(length)
+                    wait_s=True
+
+                if(wait1_s==False):
+                    wait1.append(length)
+                    wait1_s=True
+                
+                ready.append(length)
+
+
+            elif(i==new1_sum):
+                if(wait_s==False):
+                    wait.append(length)
+                    wait_s=True
+
+                if(wait1_s==False):
+                    wait1.append(length)
+                    wait1_s=True
+                
+                ready.append(length)
+
+            elif(i==new_sum):
                 if(wait_s==False):
                     wait.append(length)
                     wait_s=True
@@ -106,7 +164,8 @@ def one():
             
             else:
                bad.append(i)
-
+            
+            '''
 
 
             z=random.expovariate(1)      
@@ -121,31 +180,31 @@ def one():
     y=[]
     for i in range(len(bad)):
         y.append(1)
-    plt.plot(bad, y, linestyle='dashed', linewidth = 1, marker='o', markerfacecolor='red', markersize=3)
+    plt.plot(bad, y, linewidth = 1, marker='o', markerfacecolor='red', markersize=3)
     y=[]
     for i in range(len(ready)):
         y.append(2)
-    plt.plot(ready, y, linestyle='dashed', linewidth = 1, marker='o', markerfacecolor='red', markersize=3)
+    plt.plot(ready, y, linewidth = 1, marker='o', markerfacecolor='red', markersize=3)
     y=[]
     for i in range(len(wait1)):
         y.append(3)
-    plt.plot(wait1, y, linestyle='dashed', linewidth = 1, marker='o', markerfacecolor='red', markersize=3)
+    plt.plot(wait1, y, linewidth = 1, marker='o', markerfacecolor='red', markersize=3)
     y=[]
     for i in range(len(wait)):
         y.append(4)
-    plt.plot(wait, y, linestyle='dashed', linewidth = 1, marker='o', markerfacecolor='red', markersize=3)
+    plt.plot(wait, y, linewidth = 1, marker='o', markerfacecolor='red', markersize=3)
     y=[]
     for i in range(len(can1)):
         y.append(5)
-    plt.plot(can1, y, linestyle='dashed', linewidth = 1, marker='o', markerfacecolor='red', markersize=3)   
+    plt.plot(can1, y, linewidth = 1, marker='o', markerfacecolor='red', markersize=3)   
     y=[]
     for i in range(len(can)):
         y.append(6)
-    plt.plot(can, y, linestyle='dashed', linewidth = 1, marker='o', markerfacecolor='red', markersize=3)
+    plt.plot(can, y, linewidth = 1, marker='o', markerfacecolor='red', markersize=3)
     y=[]
     for i in range(len(order)):
         y.append(7)
-    plt.plot(order, y, linestyle='dashed', linewidth = 1, marker='o', markerfacecolor='red', markersize=3)   
+    plt.plot(order, y, linewidth = 1, marker='o', markerfacecolor='red', markersize=3)   
 
 
     plt.show()
